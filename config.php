375061b1cd6e9022127003bdb0f08e4c417ded5a
Rahul.R <rahulraveendran06@gmail.com>
2019-10-13 11:43:50 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Enter your Host, username, password, database below.
+$conn = mysqli_connect("localhost","root","","company");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
